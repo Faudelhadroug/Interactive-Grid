ca517440b61bfc66783cbcf3fe27ab239bdb6c99
Faudel <hadroug.faudel@gmail.com>
2024-05-20 12:18:48 +0200

    feat: interactive grid v1 with asearch algo, drag start and end and wall detections

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+import Grid from './components/Grid.vue'
 </script>
 
 <template>
-  <div>
-    <h1 class="font-bold text-3xl">Hello world</h1>
+  <div class="bg-blue-300 w-screen">
+    <h1 class="font-bold text-3xl select-none text-center py-4">
+      Interactive grid
+    </h1>
+    <Grid />
   </div>
 </template>
